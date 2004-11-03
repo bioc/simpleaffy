2a993cd3ff1ff0bf3b9adae6618e733fbe0baa33
Jeff Gentry <jgentry@jimmy.harvard.edu>
2004-11-03 22:29:44 +0000

    adding new version
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/simpleaffy@10460 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/trad.scatter.plot.R
+++ R/trad.scatter.plot.R
@@ -1,17 +1,19 @@
 "trad.scatter.plot" <-
-function(x,y,add=FALSE,fc.lines=log2(c(2,4,6,8)),draw.fc.lines=TRUE,draw.fc.line.labels=TRUE,fc.line.col="lightgrey",pch=20,...) {
+function(x,y,add=FALSE,fc.lines=log2(c(2,4,6,8)),draw.fc.lines=TRUE,draw.fc.line.labels=TRUE,fc.line.col="lightgrey",pch=20,xlim=NULL,ylim=NULL,...) {
    if(!add) {
      mx <- max(x,y);     
      mn <- min(x,y);
-     plot(x,y,xlim=range(mn,mx),ylim=range(mn,mx),pch=pch,...);
+     if(is.null(xlim)) { xlim=range(mn,mx) }
+     if(is.null(ylim)) { ylim=range(mn,mx) }
+     plot(x,y,xlim=xlim,ylim=ylim,pch=pch,...);
      if(draw.fc.lines) {
        for( lne in fc.lines) {
-         xc <- c(mn,mx);
-         yc <- c(mn+lne,mx+lne);
+         xc <- xlim 
+         yc <- c(ylim[1]+lne,ylim[2]+lne);
          lines(xc,yc,col=fc.line.col); 
          if(draw.fc.line.labels) {text(mn+0.25,lne+mn,2^lne,col=fc.line.col); }
-         xc <- c(mn, mx);
-         yc <- c(mn-lne,mx-lne);
+         xc <- xlim
+         yc <- c(ylim[1]-lne,ylim[2]-lne);
          lines(xc,yc,col=fc.line.col); 
           if(draw.fc.line.labels) {text(lne+mn,mn+0.25,2^lne,col=fc.line.col); }
        }
