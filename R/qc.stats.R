ae98635e6c3bde95c95670011562ac635c066923
Crispin Miller <Cmiller@picr.man.ac.uk>
2005-01-06 18:09:23 +0000

    updated qc plot function
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/simpleaffy@10784 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/qc.stats.R
+++ R/qc.stats.R
@@ -273,7 +273,7 @@ for(no in 1:length(unnormalised)){
 
 
 
-plot.qc.stats<-function(x,fc.line.col="black",sf.ok.region="light blue",chip.label.col="black",sf.thresh = 3.0,gdh.thresh = 3.0,ba.thresh = 3.0,present.thresh=10,bg.thresh=20,label=NULL,...) {
+.plot.qc.stats2<-function(x,fc.line.col,sf.ok.region,chip.label.col,sf.thresh,gdh.thresh,ba.thresh,present.thresh,bg.thresh,label,main,usemid,...) {
 
   sfs    <- log2(sfs(x))
 
@@ -308,7 +308,7 @@ plot.qc.stats<-function(x,fc.line.col="black",sf.ok.region="light blue",chip.lab
   x2 <- (4.5 +  meansf) * cos(i * arc);
   y2 <- (4.5 +  meansf) * sin(i * arc);
 
-  plot(x1[1],y1[1],pch=".",xlim=range(-12,12),ylim=range(-12,12),xaxt="n",yaxt="n",xlab="",ylab="",col=sf.ok.region,...);
+  plot(x1[1],y1[1],pch=".",xlim=range(-12,12),ylim=range(-12,12),xaxt="n",yaxt="n",xlab="",ylab="",col=sf.ok.region,main=main,...);
 
   polygon(c(x1,rev(x2),x1[1]),c(y1,rev(y2),y1[1]),col=sf.ok.region,border=sf.ok.region);
 
@@ -326,8 +326,14 @@ plot.qc.stats<-function(x,fc.line.col="black",sf.ok.region="light blue",chip.lab
   text(0,9,"+3",col=fc.line.col)
   arc <- 2 * pi / n;
   rats <- ratios(x);
-  gdh <- rats[,2];
-  ba  <- rats[,1];
+  if(!usemid) {
+    gdh <- rats[,2];
+    ba  <- rats[,1];
+  }
+  else {  
+    gdh <- rats[,4];
+    ba  <- rats[,3];
+  }
   bb  <- x@bioBCalls
 
   for(i in 1:n) {
@@ -364,10 +370,155 @@ plot.qc.stats<-function(x,fc.line.col="black",sf.ok.region="light blue",chip.lab
        text(x2,y2,label="bioB",col="red");
      }
   }
-  legend(-11,12,pch=1:2,colnames(rats)[1:2])
+  if(!usemid) {
+    legend(-11,12,pch=2:1,colnames(rats)[1:2])
+  }
+  else {
+    legend(-11,12,pch=2:1,colnames(rats)[3:4])
+  }
 }
 
+plot.qc.stats<-function(x,fc.line.col="black",sf.ok.region="light blue",chip.label.col="black",sf.thresh = 3.0,gdh.thresh = 1.25,ba.thresh = 3.0,present.thresh=10,bg.thresh=20,label=NULL,main="QC Stats",usemid=F,spread=c(-8,8),type="l",...) {
+  if(type=="c") { 
+    .plot.qc.stats2(x,fc.line.col,sf.ok.region,chip.label.col,sf.thresh,gdh.thresh,ba.thresh,present.thresh,bg.thresh,label,main,usemid,...) 
+    return()
+  }
+  old.par <- par()
+  par(mai=c(0,0,0,0))
+  sfs    <- log2(sfs(x))
+
+  n      <- length(sfs)
+
+  meansf <- mean(sfs)
+
+  dpv <- percent.present(x)
+  dpv <- (round(100*dpv))/100;
+
+  abg <- avbg(x)
+  abg <- (round(100*abg))/100;
+	
+  if(is.null(label)) { label <- names(maxbg(x)) }
+  d1 <- 0.0;
+  d2 <- 0.0;
+  d3 <- 0.0;
+
+  for(i in 1:n) {
+    for(j in 1:n) { 
+      d1 <- max(abs(sfs[i] - sfs[j]),d1);
+      d2 <- max(abs(dpv[i] - dpv[j]),d1);
+      d3 <- max(abs(abg[i] - abg[j]),d3);
+    }
+  }
+
+  # set up plotting area - a column for array names next to a column for the QC
+
+  m <- matrix(c(4,2,1,3) ,nrow=2,ncol=2)
+  layout(m,c(1,2),c(0.1,1))
+  # the title
+  if(is.null(main)) { main="" }
+  plot(0,0,xlim=range(0,1),ylim=range(0,1),type="n",yaxs="i",xaxt="n",yaxt="n",bty="n")
+  text(0.5,0.5,labels=main,adj=0,cex=2)
+
+  # write out the array names
+
+  cex <- 1.0
+  while((maxwidth <- max(sapply(label,function(a) { strwidth(a,units="f",cex)} ))) >= 1) cex <- cex/maxwidth
+
+  plot(0,0,xlim=range(0,1),ylim=range(-1,n),type="n",yaxs="i",xaxt="n",yaxt="n",bty="n")
+  text(1,(1:n)-0.5,labels=label,adj=1)
+
+  plot(0,0,xlim=spread,ylim=c(-1,n),type="n",xaxs="i",yaxs="i",xaxt="n",yaxt="n",bty="n")
+
+  x1 <- (1.5 +  meansf)
+  y1 <- 0
+  x2 <- (-1.5 +  meansf)
+  y2 <- n
+
+  polygon(c(x1,x2,x2,x1),c(y1,y1,y2,y2),col=sf.ok.region,border=sf.ok.region);
+  lines(c(0,0),c(0,n),lty=1,col=fc.line.col)
+  lines(c(-1,-1),c(0,n),lty=2,col="grey")
+  lines(c(-2,-2),c(0,n),lty=2,col="grey")
+  lines(c(-3,-3),c(0,n),lty=2,col=fc.line.col)
+  lines(c(1,1),c(0,n),lty=2,col="grey")
+  lines(c(2,2),c(0,n),lty=2,col="grey")
+  lines(c(3,3),c(0,n),lty=2,col=fc.line.col)
+  text(3,-1,"3",pos=3,col=fc.line.col)
+  text(2,-1,"2",pos=3,col=fc.line.col)
+  text(1,-1,"1",pos=3,col=fc.line.col)
+  text(-3,-1,"-3",pos=3,col=fc.line.col)
+  text(-2,-1,"-2",pos=3,col=fc.line.col)
+  text(-1,-1,"-1",pos=3,col=fc.line.col)
+  text(0,-1,"0",pos=3,col=fc.line.col)
+
+  rats <- ratios(x);
+  if(!usemid) {
+    gdh <- rats[,2];
+    ba  <- rats[,1];
+  }
+  else {
+    gdh <- rats[,4];
+    ba  <- rats[,3];
+  }
+
+  bb  <- x@bioBCalls
+
+  for(i in 1:n) {
+    x1<-spread[1]
+    x2<-spread[2]
+    y1<-i-1;
+    y2<-i-1;
+    lines(c(x1,x2),c(y1,y2),lty=2,col="light grey")
+    if(d1 > sf.thresh) { col = "red" } else {col="blue"}
+     x1 <- sfs[i]
+     y1 <- i-0.25
+     lines(c(0,x1),c(y1,y1),col=col);
+
+     points(x1,y1,col=col,pch=20);
+     x2 <- gdh[i]
+     y2 <- i-0.5;
+     if(gdh[i] > gdh.thresh) { col = "red" } else {col="blue"}	
+     points(x2,y2,pch=1,col=col);
+
+     x2 <- ba[i];
+     y2 <- i-0.5;
+     if(ba[i] > ba.thresh) { col = "red" } else {col="blue"}	
+     points(x2,y2,pch=2,col=col);
+
+     if(d2 > present.thresh) { col = "red" } else {col="blue"}
+     x2 <- spread[1]
+     y2 <- i-0.25
+     dpvs<-paste(dpv[i],"%",sep="")
+     text(x2,y2,label=dpvs,col=col,pos=4);
+     if(d3 > bg.thresh) { col = "red" } else {col="blue"}
+     x2 <- spread[1]
+     y2 <- i-0.75
+     text(x2,y2,label=abg[i],col=col,pos=4);
+     if(bb[i]!="P") {
+       x2 <- (12 * cos(i * arc));
+       y2 <- (12 * sin(i * arc));
+       text(0,i-1,label="bioB",col="red");
+     }
+
+  }
+  plot(0,0,xlim=range(0,1),ylim=range(0,1),type="n",yaxs="i",xaxt="n",yaxt="n",bty="n")
+  if(!usemid) {
+    points(0.25,0.25,pch=1)
+    text(0.3,0.25,colnames(rats)[2],pos=4)
+    points(0.25,0.5,pch=2)
+    text(0.3,0.5,colnames(rats)[1],pos=4)
+  }
+  else {
+    points(0.25,0.25,pch=1)
+    text(0.3,0.25,colnames(rats)[4],pos=4)
+    points(0.25,0.5,pch=2)
+    text(0.3,0.5,colnames(rats)[3],pos=4)
+  }
+  
+  ow <- options("warn")$warn
+  options(warn=-1)
+  par(old.par)
+  options(warn=ow)
+}
+
+
 setMethod("plot","QCStats",function(x,y) plot.qc.stats(x,...))
-
-
-
