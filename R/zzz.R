a230bdaa6bb244949e259358bd0661d4007c4d16
Crispin Miller <Cmiller@picr.man.ac.uk>
2005-05-10 16:55:17 +0000

    fixes to hmap
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/simpleaffy@12263 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,7 @@
   where <- match(paste("package:", pkg, sep=""), search());
   .initClassesAndMethods(where);
   cacheMetaData(as.environment(where));
-  cat("Welcome to 'simpleaffy' V 2.10-3                             \n");
+  cat("Welcome to 'simpleaffy' V 2.11                               \n");
   cat("      Produced by The Paterson Institute for Cancer Research \n");
   cat("      and funded by CANCER RESEARCH UK.                      \n");
   cat("      http://bioinformatics.picr.man.ac.uk/simpleaffy        \n");
