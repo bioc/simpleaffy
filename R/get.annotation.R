b283d5a2b5dc18b0462b68573d1782cf8a47dc80
Crispin Miller <Cmiller@picr.man.ac.uk>
2005-07-07 12:03:14 +0000

    workaround to annotation retrieval issue that arose when probeset ids not present
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_1_6/madman/Rpacks/simpleaffy@13136 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/get.annotation.R
+++ R/get.annotation.R
@@ -1,25 +1,41 @@
+.if.probeset.not.found <- function(x) {
+  return("NoAnno")
+}
 
 .strip.list <- function(x) {
   mapply(function(y,z) {
                         if(length(y) > 1) {
-                            print(paste("WARNING: '",z,"' has more than one entry in annotation list. Taking the first one."));
+                            warning(paste("'",z,"' has more than one entry in annotation list. Taking the first one."));
     	 		    print(y)
                         }
                         return(y[1]);
                       },x,names(x));
 }
 
-get.annotation <- function(x,cdfname) {
-  library(cdfname,character.only=T);
-  symb  <- .strip.list(mget(x,envir=get(paste(cdfname,"SYMBOL",sep=""))));
-  desc  <- .strip.list(mget(x,envir=get(paste(cdfname,"GENENAME",sep=""))));
-  accno <- .strip.list(mget(x,envir=get(paste(cdfname,"ACCNUM",sep=""))));
-  uni   <- .strip.list(mget(x,envir=get(paste(cdfname,"UNIGENE",sep=""))));
-  acc.lnk <- paste("=HYPERLINK(\"http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=search&db=nucleotide&term=",accno,"\",\"",accno,"\")",sep="");
-  uni.lnk <- paste("=HYPERLINK(\"http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=search&db=unigene&term=",uni,"&dopt=unigene\",\"",uni,"\")",sep="")	
-  res <- cbind(symb,acc.lnk,uni.lnk,desc);
-  colnames(res) <- c("gene name","accession","unigene","description");
-  return(res);
+
+get.annotation <- function (x, cdfname,verbose=F) {
+    library(cdfname, character.only = T)
+    symb <- .strip.list(mget(x, envir = get(paste(cdfname, "SYMBOL",
+        sep = "")),ifnotfound=list(.if.probeset.not.found)))
+    desc <- .strip.list(mget(x, envir = get(paste(cdfname, "GENENAME",
+        sep = "")),ifnotfound=list(.if.probeset.not.found)))
+    accno <- .strip.list(mget(x, envir = get(paste(cdfname, "ACCNUM",
+        sep = "")),ifnotfound=list(.if.probeset.not.found)))
+    uni <- .strip.list(mget(x, envir = get(paste(cdfname, "UNIGENE",
+        sep = "")),ifnotfound=list(.if.probeset.not.found)))
+    ok <- (symb != "NoAnno") & (desc != "NoAnno") & (accno!= "NoAnno") & (uni != "NoAnno")
+    names(ok) <- x
+    if(!ok && verbose) { warning(paste("value for '",names(ok)[ok], "' not found", sep = ""), call. = FALSE) }
+    acc.lnk <- paste("=HYPERLINK(\"http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=search&db=nucleotide&term=",
+        accno, "\",\"", accno, "\")", sep = "")
+    acc.lnk[!ok] <- "NoAnno"
+    uni.lnk <- paste("=HYPERLINK(\"http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=search&db=unigene&term=",
+        uni, "&dopt=unigene\",\"", uni, "\")", sep = "")
+    uni.lnk[!ok] <- "NoAnno"
+    res <- cbind(symb, acc.lnk, uni.lnk, desc)
+    res[res=="NoAnno"] <- "No Annotation Found"
+    colnames(res) <- c("gene name", "accession", "unigene", "description")
+    return(res)
 }
 
 
