0b268c92ae3e6680a5eb61e6ac31dd4abd4226ce
Crispin Miller <Cmiller@picr.man.ac.uk>
2006-04-21 09:32:21 +0000

    fixed doc bugs in get.array.indices
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/simpleaffy@17421 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -5,11 +5,6 @@
   where <- match(paste("package:", pkg, sep=""), search());
   .initClassesAndMethods(where);
   cacheMetaData(as.environment(where));
-  cat("Welcome to 'simpleaffy' V 2.5.0                              \n");
-  cat("      Produced by The Paterson Institute for Cancer Research \n");
-  cat("      and funded by CANCER RESEARCH UK.                      \n");
-  cat("      http://bioinformatics.picr.man.ac.uk/simpleaffy        \n");
-  cat("      mailto: microarray@picr.man.ac.uk                      \n");
   .createQCEnvironment()
 }
 
