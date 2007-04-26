e09c966edafe2421d7e71b142e8f08c2af81a12d
Crispin Miller <Cmiller@picr.man.ac.uk>
2007-04-26 15:22:15 +0000

    cleaned up .onLoad
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_0/madman/Rpacks/simpleaffy@24303 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,6 @@
 .onLoad <-  function(libname, pkgname) {
     require("methods", quietly=TRUE)
     require("utils", quietly=TRUE)
-    require("affy", quietly=TRUE)
-    require("genefilter", quietly=TRUE)
-    require("Biobase", quietly=TRUE)
     library.dynam("simpleaffy", pkgname, libname)
     .initClassesAndMethods()
 }
